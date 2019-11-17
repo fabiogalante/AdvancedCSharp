07eb3ae57546b809e68c01dd3669c4831fa5345c
Galante <fabiogalantemans@gmail.com>
2019-11-17 12:08:43 -0300

    Delegates

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -6,7 +6,40 @@ namespace Delegates
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Delegates
+            // -  An object that knows how to call a method (or a group of methods)
+            // -  A reference to a function
+
+
+            // Why  do we need delegates?
+
+            // - For designing extensible and flexible applications (eg frameworks)
+
+
+
+            var processor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+            //PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;
+            Action<Photo> filterHandler = filters.ApplyBrightness;
+
+            //Multi CastDelegate
+            filterHandler += filters.Resize;
+            filterHandler += RemoveRedEyeFilter;
+
+
+            processor.Process("photo.jpg", filterHandler);
+
         }
+
+
+        //Other exemple
+
+        static void RemoveRedEyeFilter(Photo photo)
+        {
+            Console.WriteLine("Apply RemoveRedEye");
+        }
+
+
+
     }
 }
